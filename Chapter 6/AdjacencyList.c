f5b0665a7cb56629fc604c95fcd157e0f8028691
Wu-wzh <3343369159@qq.com>
2023-04-24 16:28:20 +0800

    4.24

--- Chapter 6/AdjacencyList.c
+++ Chapter 6/AdjacencyList.c
@@ -2,7 +2,7 @@
 #include <windows.h>
 #define true 1
 #define false 0
-#define MaxVertexNum 5
+#define MaxVertexNum 10
 
 typedef struct ArcNode{ //就是个链表
     int adjvex; //这个链表节点指向那个顶点
@@ -17,6 +17,7 @@ typedef struct VEle{ //叫元素好一点，改名后续不影响
 typedef struct{
     AdjList vertices; //AdjList是一个VNode的顺序表
     int vexnum, arcnum;
+    int isdir;
 } ALGraph;
 
 void CreateList(ArcNode** first, int vex[], int len){ //len为vex的长度
@@ -49,27 +50,84 @@ void InitGraph(ALGraph *G){
 }
 
 void CreateGraph(ALGraph *G){
-    G->vertices[0].data = 'A';
-    G->vertices[1].data = 'B';
-    G->vertices[2].data = 'C';
-    G->vertices[3].data = 'D';
-    G->vertices[4].data = 'E'; //顶点
+    printf("请输入顶点数和边数（用空格隔开）：");
+    scanf("%d%d", &G->vexnum, &G->arcnum);
+    // 输入每个顶点的信息
+    printf("请依次输入每个顶点的信息：\n");
+    for (int i = 0; i < G->vexnum; i++){
+        printf("顶点[%d]为：", i);
+        scanf("%s", &G->vertices[i].data);
+    }
 
-    int x1[3] = {1,2,3};
-    CreateList(&(G->vertices[0].first), x1, 3);
-    int x2[2] = {0,4};
-    CreateList(&(G->vertices[1].first), x2, 2);
-    int x3[4] = {0,1,2,3};
-    CreateList(&(G->vertices[2].first), x3, 4);
-    int x4[1] = {2};
-    CreateList(&(G->vertices[3].first), x4, 1);
-    int x5[3] = {0,3,4};
-    CreateList(&(G->vertices[4].first), x5, 3);
+    //无向图还是有向图
+    printf("无向图或有向图（0无向图，1有向图）：\n");
+    scanf("%d", &G->isdir);
+
+    for (int j = 0; j < G->vexnum; j++){
+        printf("第%d个顶点共有几条边：", j);
+        int len;
+        scanf("%d", &len);
+        int vex[len];
+        printf("依次输入与第%d个顶点邻接的顶点：\n");
+        for (int k = 0; k < len; k++){
+            scanf("%d", &vex[k]);
+        }
+        CreateList(&(G->vertices[j].first), vex, len);
+    }
     
 }
 
+//判断两个节点是否相邻 x y 为节点的标号，从0开始(只实现了无向图，有向图需要进行两遍如下操作)
+int Adjacent(ALGraph G, int x, int y){
+    ArcNode* p = G.vertices[x].first;
+    while(p != NULL){
+        if (p->adjvex == y) return true;
+        p = p->next;
+    }
+    return false;
+}
+
+//输出一个节点所有的边
+//
+void Neighbors(ALGraph G, int x){
+    if (G.isdir == 0){ //无向图 直接找到此节点的链表遍历即可
+        ArcNode* p = G.vertices[x].first;
+        while(p != NULL){
+            printf("%d ",p->adjvex);
+            p = p->next;
+        }
+    }
+
+    //有向图 不仅要找到此节点的链表，还要找到其他节点的链表中是否有自己
+    //时间复杂度较高，取决于边的条数和顶点的个数，但在稀疏矩阵中和无向图差异不大
+    else{
+        for (int i = 0; i < G.vexnum; i++){
+            //若为自己的链表，输出所有
+            //此if为寻找出边
+            if (i == x) {
+                ArcNode* p = G.vertices[i].first;
+                while(p != NULL){ 
+                    printf("%d ",p->adjvex);
+                    p = p->next;
+                }
+            }
+            //此else为寻找入边，可以看到，邻接表存取时，寻找入边需要遍历整个表，时间复杂度为O(|E|)
+            else{
+                ArcNode* p = G.vertices[i].first;
+                while(p != NULL){ 
+                    if (p->adjvex == x){
+                        printf("%d ", i);
+                        break;
+                    }
+                    p = p->next;
+                }
+            }
+        }
+    }
+}//O(|V| + |E|)
+
 void PrintAdjList(ALGraph G){
-    for (int i = 0; i < MaxVertexNum; i++){
+    for (int i = 0; i < G.vexnum; i++){
         printf("%c : ",G.vertices[i].data);
         ArcNode* p = G.vertices[i].first;
         while (p != NULL)
@@ -86,6 +144,9 @@ int main(){
     InitGraph(&G);
     CreateGraph(&G);
     PrintAdjList(G);
+    // printf("%d\n", Adjacent(G, 0, 2));
+    // printf("%d\n", Adjacent(G, 0, 4));
+    Neighbors(G, 0);
     system("pause");
     return 0;
 }
--- Chapter 6/AdjacencyMatrix.c
+++ Chapter 6/AdjacencyMatrix.c
@@ -2,12 +2,13 @@
 #include <windows.h>
 #define true 1
 #define false 0
-#define MaxVertexNum 5
+#define MaxVertexNum 10
 #define Infinte 10000;
 typedef struct{
     char Vex[MaxVertexNum]; //用来存储顶点信息
     int Edge[MaxVertexNum][MaxVertexNum]; //用来存储边信息 在存储无权图时，可不使用int，避免空间的浪费
     int vexnum, arcnum;//存储顶点和边的数量
+    int isdir; //是否是有向图
 } MGraph;
 /*此存储结构的时间复杂度：
 存储顶点结构O(n),存储边结构O(n^2),所以总时间复杂度为O(n^2)
@@ -27,27 +28,59 @@ void InitGraph(MGraph *G){
     }
 }
 void CreateGraph(MGraph *G){
-    char List[MaxVertexNum];
-    List[0] = 'A';
-    List[1] = 'B';
-    List[2] = 'C';
-    List[3] = 'D';
-    List[4] = 'E';
-    for (int i = 0; i < MaxVertexNum; i++){
-        G->Vex[i] = List[i];
-        G->Edge[0][1] = 1;
-        G->Edge[1][0] = 1;
-        G->Edge[1][2] = 1;
-        G->Edge[2][1] = 1;
-        G->Edge[2][3] = 1;
-        G->Edge[3][2] = 1;
-        G->Edge[3][4] = 1;
-        G->Edge[4][3] = 1;
-    }//生成一个环
+    // 初始化图的边
+    InitGraph(G);
+
+    printf("请输入顶点数和边数（用空格隔开）：");
+    scanf("%d%d", &G->vexnum, &G->arcnum);
+    // 输入每个顶点的信息
+    printf("请依次输入每个顶点的信息：\n");
+    for (int i = 0; i < G->vexnum; i++){
+        printf("Vex[%d] = ", i);
+        scanf("%s", &G->Vex[i]);
+    }
+    //无向图还是有向图
+    printf("无向图或有向图（0无向图，1有向图）：\n");
+    scanf("%d", &G->isdir);
+
+    // 输入每条边的起点和终点
+    printf("请依次输入每条边的起点和终点（用空格隔开,编号从0开始）：\n");
+    for (int i = 0; i < G->arcnum; i++){
+        int v1, v2;
+        printf("输入存在边的两个顶点(有向图只需要输入一次)", i);
+        scanf("%d%d", &v1, &v2);
+        G->Edge[v1][v2] = 1;
+        if (G->isdir == 0) G->Edge[v2][v1] = 1;
+    }
 }
+
+
+//判断两个节点是否相邻 x y 为节点的标号，从0开始 
+//若使用节点名称，需要有一个名称到标号的转移过程，需要消耗更多的时间复杂度
+int Adjacent(MGraph G, int x, int y){
+    return G.Edge[x][y];
+}//O(1)
+
+//输出一个节点所有的边
+void Neighbors(MGraph G, int x){
+    //有向图和无向图的处理方式不一样
+    if (G.isdir == 0){
+        for (int i = 0; i < G.vexnum; i++)
+        if (G.Edge[x][i] == 1) printf("%d ", i);
+        printf("\n");
+    }
+    else{
+        for (int i = 0; i < G.vexnum; i++){ //有向图需要列和行同时遍历
+            if (G.Edge[x][i] == 1) printf("%d ", i);
+            if (G.Edge[i][x] == 1) printf("%d ", i);
+        }
+        printf("\n");
+    }
+}//O(|V|)
+
 void PrintGraph(MGraph G){
-    for (int i = 0; i < MaxVertexNum; i++){
-        for (int j = 0; j < MaxVertexNum; j++)
+    for (int i = 0; i < G.vexnum; i++){
+        for (int j = 0; j < G.vexnum; j++)
             printf("%d ", G.Edge[i][j] == 1 ? G.Edge[i][j]:0);
         printf("\n");
     }
@@ -57,6 +90,7 @@ int main(){
     InitGraph(&G);
     CreateGraph(&G);
     PrintGraph(G);
+    Neighbors(G, 0);
     system("pause");
     return 0;
 }
